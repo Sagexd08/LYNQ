605da480a177b67aa4ca555f949423c2ebf91cfe
deep-ghosh <deepghosh821@gmail.com>
2025-07-06 11:59:58 +0530

    issue resolved

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello from LYNQ App</h1>
+        <p>This is a React component.</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
