ed857c97472a4f31df8cecd7165b2e301c7456f8
deep-ghosh <deepghosh821@gmail.com>
2025-07-06 12:13:15 +0530

    lynq issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// Import CSS in order of specificity
 import './index.css';
-import App from './App';
+import './textures.css';
+import './mobile-fix.css';
+
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
